b81f8aa92479bbbb506eec16364b0686934d7913
Sergio L. Benítez D <sl.benitezd@suabochica.com>
2018-04-25 14:08:39 +0200

    Add images to editors and journals page

--- src/components/pages/EditorsAndJournals.vue
+++ src/components/pages/EditorsAndJournals.vue
@@ -4,7 +4,7 @@
       <h3 class="display-3">Editors and Journals</h3>
       <v-divider class="my-3"></v-divider>
       <section>
-        <blockquote>Making your journal's reviews more reliable using ROSES</blockquote>
+        <p>Making your journal's reviews more reliable using ROSES</p>
         <p>ROSES forms can be integrated into the workflow of any journal, ensuring that published reviews report their activities to a high standard. This can substantially increase the validity and impact of your journal.</p>
         <p>See the information below to find out more about using ROSES in editorial and peer-review processes.</p>
         <p>Contact us if you would like to know more about digitally integrating ROSES into your journal's workflow.</p>
@@ -13,7 +13,7 @@
         <v-layout row>
           <v-flex xs12 md4>
             <v-card>
-              <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+              <v-card-media src="/static/images/roses.png" height="200px">
               </v-card-media>
               <v-card-title primary-title>
                 <h3 class="headline mb-0">How can ROSES save you resources?</h3>
@@ -26,7 +26,7 @@
 
           <v-flex xs12 md4>
             <v-card>
-              <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+              <v-card-media src="/static/images/roses.png" height="200px">
               </v-card-media>
               <v-card-title primary-title>
                 <h3 class="headline mb-0">Why choose ROSES?</h3>
@@ -39,7 +39,7 @@
 
           <v-flex xs12 md4>
             <v-card>
-              <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+              <v-card-media src="/static/images/roses.png" height="200px">
               </v-card-media>
               <v-card-title primary-title>
                 <h3 class="headline mb-0">Journals already using ROSES</h3>
