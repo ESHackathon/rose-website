7dd5140849a346f92980090d501ff08f9243fa94
Sergio L. Benítez D <sl.benitezd@suabochica.com>
2018-04-25 11:40:03 +0200

    Add about content

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,7 +1,14 @@
 <template>
   <v-layout row>
     <v-flex sm12 md8 offset-md2>
-      <p>About</p>
+      <h3 class="display-3">Why ROSES?</h3>
+      <v-divider class="my-3"></v-divider>
+      <p>Reliable summaries of the evidence are vital in order to facilitate evidence-informed decision making in conservation and environmental management. The gold standard in evidence synthesis is widely accepted as systematic reviews and related systematic maps. As more systematic reviews and maps are published it is clear that there is a variability in how well these reviews are conducted and reported. </p>
+      <p>We introduce ROSES (RepOrting standards for Systematic Evidence Syntheses) forms and flow diagram, designed specifically for systematic reviews and maps in the field of conservation and environmental management. </p>
+      <p>ROSES forms can be used during the preparation of systematic review and map protocols and final reports. They make it easier to write these documents by prompting the user to ensure they have included the right information with the correct level of detail.</p>
+​      <p>The ROSES forms are a checklist of details that should be reported in review documentation. They ensure that all necessary content required by the CEE Guidelines for Systematic Reviews in Environmental Management is present and described in detail. ROSES can prevent time- and resource- consuming bounces of manuscripts before being sent out for peer-review.</p>
+      <p>ROSES forms facilitate rapid identification of key conduct-related review information by journal editors, peer-reviewers and readers. This can speed up the time needed for a manuscript to be considered by a journal and make life easier for peer-reviewers and editors.</p>
+      <p>Finally, the ROSES forms can help to drive up the quality of systematic reviews and maps by ensuring that review authors need to report methods for major steps in the review process. This transparency makes it hard to hide substandard methods.</p>
     </v-flex>
   </v-layout>
 </template>
